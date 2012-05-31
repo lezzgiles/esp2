b92c9b06d05c92e2765a8c837be340637fad268c
Lezz Giles <lezzgiles@gmail.com>
2012-05-30 22:42:34 -0400

    start of refactoring

--- html/js/index.js
+++ html/js/index.js
@@ -6,6 +6,78 @@ esp = {
 };
 
 ///////////////////////////////////////////////////////////////////////////////
+function tell(type,details) {
+    forEach (esp.windows, function(window) {
+	    if (!window.document) { return; }
+	    forEach(window.document.getElementsByName(type+'Table'), function(table) {
+		    table.setRow(details);
+		});
+	});
+}
+function tellLocation(locationDetails) {
+    forEach (esp.windows, function(window) {
+	    if (!window.document) { return; }
+	    forEach(window.document.getElementsByName('locationTable'), function(table) {
+		    table.setRow(locationDetails);
+		});
+	});
+}
+
+function tellTag(tagDetails) {
+    forEach (esp.windows, function(window) {
+	    if (!window.document) { return; }
+	    forEach(window.document.getElementsByName('tagTable'), function(table) {
+		    table.setRow(tagDetails);
+		});
+	});
+}
+
+function tellUser(userDetails) {
+    forEach (esp.windows, function(window) {
+	    if (!window.document) { return; }
+	    forEach(window.document.getElementsByName('userTable'), function(table) {
+		    table.setRow(userDetails);
+		});
+	});
+}
+
+function tellItem(itemDetails) {
+    forEach (esp.windows, function(window) {
+	    if (!window.document) { return; }
+	    forEach(window.document.getElementsByName('itemTable'), function(table) {
+		    table.setRow(itemDetails);
+		});
+	});
+}
+
+function tellBin(binDetails) {
+    forEach (esp.windows, function(window) {
+	    if (!window.document) { return; }
+	    forEach(window.document.getElementsByName('binTable'), function(table) {
+		    table.setRow(binDetails);
+		});
+	});
+}
+
+function tellTran(tranDetails) {
+    forEach (esp.windows, function(window) {
+	    if (!window.document) { return; }
+	    forEach(window.document.getElementsByName('tranTable'), function(table) {
+		    table.setRow(tranDetails);
+		});
+	});
+}
+
+function tellPurchase(purchaseDetails) {
+    forEach (esp.windows, function(window) {
+	    if (!window.document) { return; }
+	    forEach(window.document.getElementsByName('purchaseTable'), function(table) {
+		    table.setRow(purchaseDetails);
+		});
+	});
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // Location handling
 
 function getLocations() {
@@ -21,15 +93,6 @@ function getLocations() {
 handle.location = function (locationDetails) {
     esp.location[locationDetails.locationId] = locationDetails;
     tellLocation(locationDetails);
-}
-
-function tellLocation(locationDetails) {
-    forEach (esp.windows, function(window) {
-	    if (!window.document) { return; }
-	    forEach(window.document.getElementsByName('locationTable'), function(table) {
-		    table.setRow(locationDetails);
-		});
-	});
 }
 
 function addLocation(locationDetails,handlers) {
@@ -63,15 +126,6 @@ handle.tag = function (tagDetails) {
     tellTag(tagDetails);
 }
 
-function tellTag(tagDetails) {
-    forEach (esp.windows, function(window) {
-	    if (!window.document) { return; }
-	    forEach(window.document.getElementsByName('tagTable'), function(table) {
-		    table.setRow(tagDetails);
-		});
-	});
-}
-
 function addTag(tagDetails,handlers) {
     doit('addTag',tagDetails,handlers);
 }
@@ -96,15 +150,6 @@ function getUsers() {
 handle.user = function (userDetails) {
     esp.user[userDetails.userId] = userDetails;
     tellUser(userDetails);
-}
-
-function tellUser(userDetails) {
-    forEach (esp.windows, function(window) {
-	    if (!window.document) { return; }
-	    forEach(window.document.getElementsByName('userTable'), function(table) {
-		    table.setRow(userDetails);
-		});
-	});
 }
 
 function addUser(userDetails,handlers) {
@@ -148,15 +193,6 @@ handle.item = function (itemDetails) {
     tellItem(itemDetails);
 }
 
-function tellItem(itemDetails) {
-    forEach (esp.windows, function(window) {
-	    if (!window.document) { return; }
-	    forEach(window.document.getElementsByName('itemTable'), function(table) {
-		    table.setRow(itemDetails);
-		});
-	});
-}
-
 function addItem(itemDetails,handlers) {
     doit('addItem',itemDetails,handlers);
 }
@@ -191,15 +227,6 @@ handle.bin = function (binDetails) {
     tellBin(binDetails);
 }
 
-function tellBin(binDetails) {
-    forEach (esp.windows, function(window) {
-	    if (!window.document) { return; }
-	    forEach(window.document.getElementsByName('binTable'), function(table) {
-		    table.setRow(binDetails);
-		});
-	});
-}
-
 function addBin(binDetails,handlers) {
     doit('addBin',binDetails,handlers);
 }
@@ -223,16 +250,6 @@ handle.tran = function(tranDetails) {
     tellTran(tranDetails);
 }
 
-function tellTran(tranDetails) {
-    forEach (esp.windows, function(window) {
-	    if (!window.document) { return; }
-	    forEach(window.document.getElementsByName('tranTable'), function(table) {
-		    table.setRow(tranDetails);
-		});
-	});
-}
-
-
 function getPurchases() {
     if ('purchase' in esp) {
 	forEach (esp.purchase, tellPurchase);
@@ -240,15 +257,6 @@ function getPurchases() {
 	//esp.purchase = {};
 	doit('get',{type:'purchase'});
     }
-}
-
-function tellPurchase(purchaseDetails) {
-    forEach (esp.windows, function(window) {
-	    if (!window.document) { return; }
-	    forEach(window.document.getElementsByName('purchaseTable'), function(table) {
-		    table.setRow(purchaseDetails);
-		});
-	});
 }
 
 function addPurchase(purchaseDetails,handlers) {
@@ -268,109 +276,68 @@ function clickLogin() {
     
 }
 
+var windowDetails = {
+    location: { height: 700, width: 600, title: 'locations', userType: 1 },
+    purchase: { height: 700, width: 600, title: 'purchases', userType: 1 },
+    bin: { height: 700, width: 600, title: 'bins', userType: 1 },
+    item: { height: 700, width: 1100, title: 'items', userType: 1 },
+    admin: { height: 700, width: 1100, title: 'admin', userType: 2 },
+    tag: { height: 700, width: 1100, title: 'tag', userType: 1 },
+    changeMyPassword: { height: 240, width: 600, title: 'Change my password', userType: 0 },
+}
+
 function clickLogout() {
     document.getElementById('password').value = '';
     database = '';
     username = '';
     password = '';
-    document.getElementById('locationsButton').disabled = 'disabled';
-    document.getElementById('purchaseButton').disabled = 'disabled';
-    document.getElementById('itemButton').disabled = 'disabled';
-    document.getElementById('binsButton').disabled = 'disabled';
-    document.getElementById('adminButton').disabled = 'disabled';
-    document.getElementById('changeMyPasswordButton').disabled = 'disabled';
-    document.getElementById('loginButton').disabled = '';
-    document.getElementById('logoutButton').disabled = 'disabled';
+    forEach (Object.keys(windowDetails), function(w) { document.getElementById(w+'Button').disabled = true });
 
-    document.getElementById('database').disabled = '';
-    document.getElementById('username').disabled = '';
-    document.getElementById('password').disabled = '';
+    document.getElementById('loginButton').disabled = false;
+    document.getElementById('logoutButton').disabled = true;
+
+    document.getElementById('database').disabled = false;
+    document.getElementById('username').disabled = false;
+    document.getElementById('password').disabled = false;
 
     forEach(esp.windows,function(w) { w.close() });
 
     // Empty the database
-    esp = {
-	windows: {},
-    };
+    esp = { windows: {} };
 }
 
 handle.login = function(myUserType) {
     userType = myUserType;
     // Login worked - enable the buttons
-    document.getElementById('locationsButton').disabled = '';
-    document.getElementById('purchaseButton').disabled = '';
-    document.getElementById('itemButton').disabled = '';
-    document.getElementById('tagButton').disabled = '';
-    document.getElementById('binsButton').disabled = '';
-    if (userType == 2) {
-	document.getElementById('adminButton').disabled = '';
-    }
-    document.getElementById('changeMyPasswordButton').disabled = '';
-    document.getElementById('loginButton').disabled = 'disabled';
-    document.getElementById('logoutButton').disabled = '';
-    
-    document.getElementById('database').disabled = 'disabled';
-    document.getElementById('username').disabled = 'disabled';
-    document.getElementById('password').disabled = 'disabled';
+    forEach (Object.keys(windowDetails), function(w) {
+	    if (windowDetails[w].userType <= userType) {
+		document.getElementById(w+'Button').disabled = false;
+	    }
+	});
 
-    var loggedInMessage = document.getElementById('loggedInMessage');
-    loggedInMessage.removeChild(loggedInMessage.childNodes[0]);
-    loggedInMessage.insertBefore(document.createTextNode("Logged in to "+database+" as "+username),loggedInMessage.childNodes[0]);
+    document.getElementById('loginButton').disabled = true;
+    document.getElementById('logoutButton').disabled = false;
+    
+    document.getElementById('database').disabled = true;
+    document.getElementById('username').disabled = true;
+    document.getElementById('password').disabled = true;
+
+    document.getElementById('loggedInMessage').value = "Logged in to "+database+" as "+username;
 }
 
 window.onload = function() {
     var specs = "titlebar=no,location=no,menubar=no,status=yes,toolbar=no,scrollbars=yes";
     document.getElementById('loginButton').onclick = clickLogin;
     document.getElementById('logoutButton').onclick = clickLogout;
-    document.getElementById('locationsButton').onclick = function() {
-	if (esp.windows.location) {
-	    esp.windows.location.focus();
-	} else {
-	    esp.windows.location = window.open("location.html","locations",specs+',height=700,width=600');
-	}
-    }
-    document.getElementById('purchaseButton').onclick = function() {
-	if (esp.windows.purchase) {
-	    esp.windows.purchase.focus();
-	} else {
-	    esp.windows.purchase = window.open("purchase.html","purchases",specs+',height=700,width=600');
-	}
-    }
-    document.getElementById('binsButton').onclick = function() {
-	if (esp.windows.bin) {
-	    esp.windows.bin.focus();
-	} else {
-	    esp.windows.bin = window.open("bin.html","bins",specs+',height=700,width=600');
-	}
-    }
-    document.getElementById('itemButton').onclick = function() {
-	if (esp.windows.item) {
-	    esp.windows.item.focus();
-	} else {
-	    esp.windows.item = window.open("item.html","items",specs+',height=700,width=1100');
-	}
-    }
-    document.getElementById('adminButton').onclick = function() {
-	if (esp.windows.admin) {
-	    esp.windows.admin.focus();
-	} else {
-	    esp.windows.admin = window.open("admin.html","admin",specs+',height=700,width=600');
-	}
-    }
-    document.getElementById('tagButton').onclick = function() {
-	if (esp.windows.tag) {
-	    esp.windows.tag.focus();
-	} else {
-	    esp.windows.tag = window.open("tag.html","tag",specs+',height=700,width=600');
-	}
-    }
-    document.getElementById('changeMyPasswordButton').onclick = function() {
-	if (esp.windows.changeMyPassword) {
-	    esp.windows.changeMyPassword.focus();
-	} else {
-	    esp.windows.changeMyPassword = window.open("changeMyPassword.html","changeMyPassword",specs+',height=240,width=600');
-	}
-    }
+    forEach (Object.keys(windowDetails), function(w) { 
+	    document.getElementById(w+'Button').onclick = function() {
+		if (esp.windows[w]) {
+		    esp.windows[w].focus();
+		} else {
+		    esp.windows[w] = window.open(w+".html",windowDetails[w].title,specs+',height='+windowDetails[w].height+',width='+windowDetails[w].width);
+		}
+	    }
+	});
     document.getElementById('debugButton').onclick = function() {
 	esp.windows.debug = window.open("debug.html","debug",specs+',height=520,width=650');
     }
